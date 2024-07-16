1a0cf2bdc3782cfa744c71453f0332467e08c146
Marcell Souza <marcell.ramos@gmail.com>
2024-07-16 17:10:13 -0400

    chore: add class 2 end

---
+++ src/components/button.tsx
@@ -0,0 +1,48 @@
+// import { Settings2 } from 'lucide-react'
+import { ComponentProps, ReactNode } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
+
+const buttonVariants = tv({
+  base: 'rounded-lg px-5 font-medium flex items-center justify-center gap-2',
+
+  variants: {
+    variant: {
+      primary: 'bg-lime-300 text-lime-950 hover:bg-lime-400',
+      secondary: 'bg-zinc-800 text-zinc-200 hover:bg-zinc-700',
+      colorRed: 'bg-red-800 text-red-200 hover:bg-red-700',
+    },
+
+    size: {
+      default: 'py-2',
+      full: 'w-full h-11',
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'default',
+  },
+})
+
+// interface ButtonProps extends ComponentProps<'button'> extends {
+//   children: ReactNode
+//   variant: "primary" | "secondary"
+// }
+interface ButtonProps
+  extends ComponentProps<'button'>,
+    VariantProps<typeof buttonVariants> {
+  children: ReactNode
+}
+
+export function Button({ children, variant, size, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      // className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
+      // className={buttonVariants({ variant: 'colorRed' })}
+      className={buttonVariants({ variant, size })}
+    >
+      {children}
+    </button>
+  )
+}
